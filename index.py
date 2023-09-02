c1e914d2751ce143cb286c97c9b9ee448e5a3a17
Diana Marcelin <marcelindiana@yahoo.com>
2023-09-02 12:27:15 -0400

    mad lids game

---
+++ index.py
@@ -0,0 +1,9 @@
+#mad libs game
+
+color: input("Enter a color: ")
+plural_noun: input("Enter a plural noun: ")
+celebrity: input("Enter an celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
